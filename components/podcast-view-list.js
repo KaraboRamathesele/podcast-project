88fe3ea7b3baf307ec60b5db2d1885d56b6529ba
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-11-12 21:57:36 +0200

    updated

--- components/podcast-view-list.js
+++ components/podcast-view-list.js
@@ -3,31 +3,50 @@ import {
   LitElement,
   css,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js";
-import { store } from "../store.js";
+import { store, connect } from "../store.js";
+
+const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 class Component extends LitElement {
   static get properties() {
     return {
       previews: { state: true },
+      sorting: { state: true },
+      search: { state: true },
     };
   }
 
   constructor() {
     super();
-    const state = store.subscribe(this.storeChange);
-    this.storeChange(state);
+
+    this.disconnectStore = connect((state) => {
+      if (this.previews !== state.previews) {
+        this.previews = state.previews;
+      }
+      if (this.sorting !== state.sorting) {
+        this.sorting = state.sorting;
+      }
+      if (this.search !== state.search) {
+        this.search = state.search;
+      }
+    });
   }
 
-  /**
-   * @param {import('../types').state} state
-   */
-  storeChange = (state) => {
-    if (this.previews === state.previews) return;
-    this.previews = state.previews;
-  };
-
   disconnectedCallback() {
-    store.unsubscribe(this.storeChange);
+    this.disconnectStore();
   }
 
   static styles = css`
@@ -36,27 +55,78 @@ class Component extends LitElement {
     }
   `;
 
+  // constructor() {
+  //   super();
+  //   const state = store.subscribe(this.storeChange);
+  //   this.storeChange(state);
+  // }
+
+  // /**
+  //  * @param {import('../types').state} state
+  //  */
+  // storeChange = (state) => {
+  //   if (this.previews === state.previews) return;
+  //   this.previews = state.previews;
+  // };
+
+  // disconnectedCallback() {
+  //   store.unsubscribe(this.storeChange);
+  // }
+
+  // static styles = css`
+  //   li {
+  //     border: 1px solid var(--primary-blue);
+  //   }
+  // `;
+
   render() {
     /**
      * @type {import('../types').preview[]}
      */
-    const preview = this.previews;
+    const previews = this.previews;
 
-    const list = preview.map(({ title, id }) => {
+    const filteredPreviews = previews.filter((item) => {
+      if (!this.search) return true;
+      return item.title.toLowerCase().includes(this.search.toLowerCase());
+    });
+
+    const sortedPreviews = filteredPreviews.sort((a, b) => {
+      if (this.sorting === "a-z") return a.title.localeCompare(b.title);
+      if (this.sorting === "z-a") return b.title.localeCompare(a.title);
+
+      const dateA = new Date(a.updated).getTime();
+      const dateB = new Date(b.updated).getTime();
+
+      if (this.sorting === "oldest-latest") return dateA - dateB;
+      if (this.sorting === "latest-oldest") return dateB - dateA;
+
+      throw new Error("Invalid sorting");
+    });
+
+    const list = sortedPreviews.map(({ title, id, updated }) => {
+      const date = new Date(updated);
+      const day = date.getDate();
+      const month = MONTHS[date.getMonth() - 1];
+      const year = date.getFullYear();
+
       const clickHandler = () => store.loadSingle(id);
 
       return html`
         <li>
           <button @click="${clickHandler}">${title}</button>
+          <div>Updated: ${day} ${month} ${year}</div>
         </li>
       `;
     });
 
     return html`
       <h1>Podcast List</h1>
-      <ul>
-        ${list}
-      </ul>
+      <podcast-controls></podcast-controls>
+      ${list.length > 0
+        ? html`<ul>
+            ${list}
+          </ul>`
+        : html`<div>No matches</div>`}
     `;
   }
 }
--- scripts.js
+++ scripts.js
@@ -4,3 +4,4 @@ import "./components/podcast-meta.js";
 import "./components/podcast-preview.js";
 import "./components/podcast-view-list.js";
 import "./components/podcast-view-single.js";
+import "./components/podcast-controls.js";
