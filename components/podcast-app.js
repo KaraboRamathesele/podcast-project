62b5e37e7498464d5c2183d907dc799b8d335c62
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-11-12 20:51:24 +0200

    Updating components

---
+++ components/podcast-app.js
@@ -0,0 +1,52 @@
+import {
+  html,
+  LitElement,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js";
+import { store } from "../store.js";
+
+class Component extends LitElement {
+  static get properties() {
+    return {
+      phase: { state: true },
+    };
+  }
+
+  constructor() {
+    super();
+    const state = store.subscribe(this.storeChange);
+    this.storeChange(state);
+  }
+
+  /**
+   * @param {import('../types').state} state
+   */
+  storeChange = (state) => {
+    if (this.phase === state.phase) return;
+    this.phase = state.phase;
+  };
+
+  disconnectedCallback() {
+    store.unsubscribe(this.storeChange);
+  }
+
+  render() {
+    switch (this.phase) {
+      case "loading":
+        return html`<div>Loading...</div>`;
+
+      case "error":
+        return html`<div>Something went wrong!</div>`;
+
+      case "list":
+        return html`<podcast-view-list></podcast-view-list>`;
+
+      case "single":
+        return html`<podcast-view-single></podcast-view-single>`;
+
+      default:
+        throw new Error("Invalid phase");
+    }
+  }
+}
+
+customElements.define("podcast-app", Component);
---
+++ components/podcast-view-single.js
@@ -0,0 +1,58 @@
+import {
+  html,
+  LitElement,
+  css,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js";
+import { store } from "../store.js";
+
+class Component extends LitElement {
+  static get properties() {
+    return {
+      single: { state: true },
+    };
+  }
+
+  constructor() {
+    super();
+    const state = store.subscribe(this.storeChange);
+    this.storeChange(state);
+  }
+
+  /**
+   * @param {import('../types').state} state
+   */
+  storeChange = (state) => {
+    if (this.single === state.single) return;
+    this.single = state.single;
+  };
+
+  disconnectedCallback() {
+    store.unsubscribe(this.storeChange);
+  }
+
+  static styles = css`
+    h1 {
+      color: purple;
+    }
+    img {
+      width: 100px;
+      height: 100px;
+    }
+  `;
+
+  render() {
+    /**
+     * @type {import('../types').show}
+     */
+    const show = this.single;
+    const backHandler = () => store.loadList();
+
+    return html`
+      <button @click="${backHandler}">👈 BACK</button>
+      <h1>${show.title || ""}</h1>
+      <img src="${show.image}" />
+    `;
+  }
+}
+
+customElements.define("podcast-view-single", Component);
