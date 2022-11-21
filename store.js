01c15651e3f16c64e54f78fe4ab5d02654f0355b
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-11-21 23:29:50 +0200

    and an id to season.js

--- store.js
+++ store.js
@@ -72,6 +72,33 @@ class Store {
     });
   }
 
+  //
+
+  /**
+   * @param {string} id
+   */
+  async loadSingle(id) {
+    this.update({
+      phase: "loading",
+    });
+
+    if (!id) throw new Error('"id" is required');
+    const response = await fetch(`https://podcast-api.netlify.app/id/${id}`);
+
+    if (!response.ok) {
+      return this.update({
+        phase: "error",
+      });
+    }
+
+    const data = await response.json();
+
+    return this.update({
+      phase: "single",
+      single: data,
+    });
+  }
+
   /**
    * @param {Partial<import('./types').state>} newState
    */
