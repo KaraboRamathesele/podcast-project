3c48ad608dc79b483e7a75da94177dde3e9238c5
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-11-21 23:19:18 +0200

    added season component to scripts

--- scripts.js
+++ scripts.js
@@ -1,13 +1,9 @@
 import { getData } from "./components/api.js";
 import "./components/podcast-app.js";
-import "./components/podcast-episode.js";
 import "./components/podcast-view-list.js";
 import "./components/podcast-view-single.js";
 import "./components/podcast-controls.js";
+import "./components/podcast-seasons.js";
 
-
-let showAPI = "https://podcast-api.netlify.app/shows"
+let showAPI = "https://podcast-api.netlify.app/shows";
 let showData = await getData(showAPI);
-
-
-
