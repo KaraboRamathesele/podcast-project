dc7fa58d85ef66bf0ed2d7b5488bbfe1ca352d7c
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-11-22 00:01:04 +0200

    remove get data

--- store.js
+++ store.js
@@ -21,7 +21,7 @@ class Store {
 
   async loadList() {
     // this loadlist back to the view ? MV-because of this ?C
-    console.log("Here");
+    // console.log("Here");
     if (this.state.previews.length > 0) {
       return this.update({
         single: null,
@@ -95,7 +95,7 @@ class Store {
 
     return this.update({
       phase: "seasons", //change this from single --> seasons
-      single: data,
+      seasons: data,
     });
   }
 
